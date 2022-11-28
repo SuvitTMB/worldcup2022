b5fec7dac38de817efc70b45e76a7e7ce53ed6c1
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2022-11-28 10:05:26 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ var dateString = "";
 $(document).ready(function () {
 
   sessionStorage.clear(); 
+/*
   var str = "";
   var sLineID = "Ua6b6bf745bd9bfd01a180de1a05c23b3";
   var sLineName = "Website";
@@ -17,14 +18,14 @@ $(document).ready(function () {
   str += '<div class="NameLine">'+ sessionStorage.getItem("LineName")+'</div>';
   $("#MyProfile").html(str);  
   Connect_DB();
-   
-  //main();
+*/   
+  main();
 });
 
 
 
 async function main() {
-  await liff.init({ liffId: "1657509542-pqGkM6l2" });
+  await liff.init({ liffId: "1657509542-Oxq2d9pr" });
   document.getElementById("isLoggedIn").append(liff.isLoggedIn());
   if(liff.isLoggedIn()) {
     getUserProfile();
